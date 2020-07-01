05083a47eb380c4d89b04c03ca86b7084f94f026
Amar Khoirudin <amar.khoi25@gmail.com>
2020-07-02 06:55:08 +0700

    Deploy Heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const flash = require("connect-flash");
 const cors = require("cors");
 // Import Mongoose
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/db_staycation", {
+mongoose.connect("mongodb+srv://amarkhoirudin:server-staycation@cluster0.ty3sn.mongodb.net/db_staycation?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
